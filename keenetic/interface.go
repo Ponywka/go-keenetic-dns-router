38b9158ae5d401c602131b3497c0ba5e891c3b42
Vladimir Avtsenov <vladimir.lsk.cool@gmail.com>
2023-10-09 19:17:32 +0300

    Rename InterfaceName to Name in InterfaceBase

--- keenetic/interface.go
+++ keenetic/interface.go
@@ -2,13 +2,13 @@ package keenetic
 
 // TODO: Type for summary
 type InterfaceBase struct {
-	Id            string   `json:"id"`
-	Index         int      `json:"index"`
-	InterfaceName string   `json:"interface-name"`
-	Type          string   `json:"type"`
-	Traits        []string `json:"traits"`
-	Link          string   `json:"link"`
-	Summary       struct {
+	Id      string   `json:"id"`
+	Index   int      `json:"index"`
+	Name    string   `json:"interface-name"`
+	Type    string   `json:"type"`
+	Traits  []string `json:"traits"`
+	Link    string   `json:"link"`
+	Summary struct {
 		Layer struct {
 			Conf string `json:"conf"`
 			Link string `json:"link"`
