4707c262f9970569548fe8994427f3ac6ed9167a
Vladimir Avtsenov <vladimir.lsk.cool@gmail.com>
2023-10-16 18:58:57 +0300

    move ticker to struct

--- internal/app/updaterTicker.go
+++ internal/app/updaterTicker.go
@@ -14,28 +14,29 @@ type updaterTicker struct {
 	TickerReset chan time.Duration
 	TickerStop  chan bool
 	Result      chan updaterTickResult
+	ticker      *time.Ticker
 }
 
 func createUpdaterTicker(updater interfaces.Updater, d time.Duration) (ut updaterTicker) {
-	ticker := time.NewTicker(d)
 	ut = updaterTicker{
 		TickerReset: make(chan time.Duration),
 		TickerStop:  make(chan bool),
 		Result:      make(chan updaterTickResult),
+		ticker:      time.NewTicker(d),
 	}
 	go func() {
 		for {
 			select {
-			case <-ticker.C:
+			case <-ut.ticker.C:
 				res, err := updater.Tick()
 				ut.Result <- updaterTickResult{
 					Result: res,
 					Error:  err,
 				}
 			case d := <-ut.TickerReset:
-				ticker.Reset(d)
+				ut.ticker.Reset(d)
 			case <-ut.TickerStop:
-				ticker.Stop()
+				ut.ticker.Stop()
 			}
 		}
 	}()
